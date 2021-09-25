4c40f55403c115de314b905699752efdfecc045c
Ryan Ilano <ryan.ilano.690@my.csun.edu>
2021-09-25 09:24:32 -0700

    Intialize setup

--- app/assets/screens/LoginScreen.js
+++ app/assets/screens/LoginScreen.js
@@ -1,34 +1,29 @@
-import React from 'react'
-import { ImageBackground, StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 import AppButton from '../../components/AppButton';
 import colors from '../config/colors';
 export default function LoginScreen() {
-    return (
-        <ImageBackground 
-        style= {styles.background}
-        source= {require("../../assets/backround.jpg")}
-        >
-         <View style = {styles.buttonsContainer}>    
-        <AppButton title= "Login" />
-        <AppButton title= "Register" />    
-        </View>
-
-
-        </ImageBackground>
-    )
+  return (
+    <ImageBackground
+      style={styles.background}
+      source={require('../../assets/backround.jpg')}
+    >
+      <View style={styles.buttonsContainer}>
+        <AppButton title="Login" />
+        <AppButton title="Register" />
+      </View>
+    </ImageBackground>
+  );
 }
 
 const styles = StyleSheet.create({
-
-background: {
+  background: {
     flex: 1,
-    justifyContent: "flex-end",
-    alignItems: "center",  
-    
-},
-buttonsContainer: {
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  buttonsContainer: {
     padding: 20,
-    width: "80%",
-}
-
-})
+    width: '80%',
+  },
+});
--- app/components/AppButton.js
+++ app/components/AppButton.js
@@ -1,26 +1,26 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import colors from '../assets/config/colors'
-export default function AppButton({title}) {
-    return (
-        <View style= {styles.button}> 
-        <Text style={styles.text}>{title}</Text>
-        </View>
-    )
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import colors from '../assets/config/colors';
+export default function AppButton({ title }) {
+  return (
+    <View style={styles.button}>
+      <Text style={styles.text}>{title}</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    button: { 
-        backgroundColor: colors.buttons,
-        borderRadius: 20,
-        justifyContent: "center",
-        alignItems: "center",
-        padding: 15,
-        width: "100%",
-    },
-    text: {
-        color: colors.buttonText,
-        fontSize: 18,
-        fontWeight: "bold",
-    }
-})
+  button: {
+    backgroundColor: colors.buttons,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 15,
+    width: '100%',
+  },
+  text: {
+    color: colors.buttonText,
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
