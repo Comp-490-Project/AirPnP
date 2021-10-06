4e9b37e991ebeda76b5e262355df4e6c596d5bda
Ryan Ilano <ryan.ilano.690@my.csun.edu>
2021-10-06 13:15:10 -0700

    Conditional rendering of MapScreen.js if user location is not set

--- app/assets/screens/MapScreen.js
+++ app/assets/screens/MapScreen.js
@@ -1,29 +1,32 @@
-import React from "react"; 
+import React from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import MapView, {PROVIDER_GOOGLE, Marker} from "react-native-maps"; 
-import { useEffect, useState } from "react";
-import * as Location from 'expo-location';
-import useLocation from "../../hooks/useLocation";
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+import useLocation from '../../hooks/useLocation';
 
-
-export default function MapScreen(){
+export default function MapScreen() {
   const location = useLocation();
-  console.log(JSON.stringify(location));
-  return(
-    <View style={styles.container}>
-      <MapView 
-      provider={PROVIDER_GOOGLE} //Google Maps 
-      style = {styles.map}
-      showsUserLocation = {true}
-      region={{
-        latitude: location.latitude,
-        longitude: location.longitude,
-        latitudeDelta: 0.0015,
-        longitudeDelta: 0.0121,
-      }}
-      ></MapView> 
-    </View>
-  )
+
+  return (
+    <>
+      {!location ? (
+        <Text>Loading...</Text>
+      ) : (
+        <View style={styles.container}>
+          <MapView
+            provider={PROVIDER_GOOGLE} //Google Maps
+            style={styles.map}
+            showsUserLocation={true}
+            region={{
+              latitude: location.latitude,
+              longitude: location.longitude,
+              latitudeDelta: 0.0015,
+              longitudeDelta: 0.0121,
+            }}
+          ></MapView>
+        </View>
+      )}
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
