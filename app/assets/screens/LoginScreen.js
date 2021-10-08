44db9f6e69186c6803385f6af98baf2f985568c0
KARIM ATALLA <62119150+katalla1@users.noreply.github.com>
2021-10-08 12:41:24 -0700

    Update LoginScreen.js

--- app/assets/screens/LoginScreen.js
+++ app/assets/screens/LoginScreen.js
@@ -7,23 +7,13 @@ import {
   TouchableOpacity,
   
 } from 'react-native';
+
 import AppButton from '../../components/AppButton';
 import AppTextInput from '../../components/AppTextInput';
-import { Formik } from 'formik';
-import * as Yup from 'yup';
 import { Link } from 'react-router-native';
 import colors from '../../assets/config/colors';
-
-} from "react-native";
-import AppButton from "../../components/AppButton";
-import AppTextInput from "../../components/AppTextInput";
-import AppText from "../../components/AppText";
 import { Formik } from "formik";
 import * as Yup from "yup";
-import { Link } from "react-router-native";
-import RegisterScreen from "./RegisterScreen";
-import colors from "../../assets/config/colors";
-
 
 const validationSchema = Yup.object().shape({
   email: Yup.string().required().email().label('Email'),
