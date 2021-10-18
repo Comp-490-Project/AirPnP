9e6074818b2737a1127ce9d5612e18f52e9ac373
katalla1 <katalla@hotmal.com>
2021-10-18 10:34:20 -0700

    added the forgot password screen and linked to firebase

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import { NativeRouter, Route, BackButton } from 'react-router-native';
 import RegisterScreen from './app/assets/screens/RegisterScreen';
 import MapScreen from './app/assets/screens/MapScreen';
 import LoginScreen from './app/assets/screens/LoginScreen';
+import ForgotPassword from './app/assets/screens/ForgotPassword';
 
 export default function App() {
   return (
     <NativeRouter>
       <BackButton>
         <Route path="/register" component={RegisterScreen} />
+        <Route path="/forgotPassword" component={ForgotPassword} />
         <Route path="/map" component={MapScreen} />
         <Route path="/" component={LoginScreen} exact />
       </BackButton>
