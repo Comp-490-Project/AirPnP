69bf63d2750d93c42998460afbbebb6451d5fbf0
bam38652 <brian.melgar.364@my.csun.edu>
2021-10-21 12:19:00 -0700

    Add Restroom Function.

--- app/assets/screens/AddScreen.js
+++ app/assets/screens/AddScreen.js
@@ -1,6 +1,6 @@
 import React, { useEffect,useState } from "react";
 import { StyleSheet, Button, Text, View, Dimensions, SafeAreaView,Image} from "react-native";
-import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
+import MapView, { PROVIDER_GOOGLE, Marker,Circle } from "react-native-maps";
 import * as Location from 'expo-location'
 import * as Permissions from 'expo-permissions'
 import useLocation from "../../hooks/useLocation";
@@ -9,74 +9,6 @@ import markerImage from '../marker.png';
 
 
 
-/*
-function AddScreen() {
-  const location = useLocation();
-  const [marker,setMarker]=useState({});
-
-  
-
-  useEffect(()=>{
-    console.log(JSON.stringify(marker));
-  },[marker]);
-
-  const handlePress = (e) =>{
-    setMarker(e.nativeEvent.coordinate)
-  }
-
-  return (
-  <>
-      {!location ? (
-        <Text style= {styles.loadingText}>Loading...</Text>
-      ) : (
-      <View style={styles.container}>
-        <MapView
-          provider={PROVIDER_GOOGLE}
-          style={styles.map}
-          onPress={handlePress}
-          showsUserLocation={true}
-          showsMyLocationButton={true}
-          region={{
-            latitude: location.latitude,
-            longitude: location.longitude,
-            longitudeDelta: 0.0015,
-            latitudeDelta: 0.0121,
-          }}
-        >
-          {mapMarkers()}
-        </MapView> 
-        <View style={styles.addButton}>
-          <AppButton title="Add" onPress={null}/>
-        </View>    
-      </View> 
-    )}
-  </>
-  );
-}
-
-export default AddScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  addButton:{
-    position: 'absolute',
-    bottom: 50,
-    width: 200,
-    marginBottom: 10,
-    height: 10,
-
-  },
-  map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
-}); 
-
-*/
 export default function AddScreen() {
 
   const [ mapRegion, setRegion ] = useState(null)
@@ -91,14 +23,14 @@ export default function AddScreen() {
       }else{
         setLocationPermission(true);
       }
-
       let {coords:{ latitude,longitude } } = await Location.getLastKnownPositionAsync();
       setLocation( JSON.stringify( { latitude, longitude } ) )
 
-      setRegion({latitude, longitude, latitudeDelta: 0.0015, longitudeDelta: 0.0015}) //Center Map on Location fetched above.
-    }
+      setRegion({latitude, longitude, latitudeDelta: 0.0121, longitudeDelta: 0.0015}) //Center Map on Location fetched above.
+    };
+    console.log(mapRegion);
     getLocationAsync();
-  })
+  },[]);
 
   if ( location === null ) { //Loading Animations Here
       return <Text>Finding your current location...</Text>
@@ -116,26 +48,41 @@ export default function AddScreen() {
     setRegion(mapRegion)
   }
 
+  const markerHandle = mapRegion =>{
+    <Marker
+      coordinate={{
+        latitude: -118,
+        longitude:-34
+      }}
+    > 
+    </Marker>
+  }
+
   return (
     <View style={styles.container}>
       <MapView
         style={ styles.map }
         showsMyLocationButton={true}
+        showsUserLocation={true}
         initialRegion={mapRegion}
         onRegionChangeComplete={onRegionChange}
       >
+      <Circle //TODO: This needs to be responsive in regards to the latDelta/longDelta values in the MapView. 
+        center={mapRegion}
+        strokeColor = { '#1a66ff' }
+        strokeWidth = { 1 }
+        radius={20}
+        fillColor = { 'rgba(230,238,255,0.5)' }
+      ></Circle>
       </MapView>
       <View style={styles.markerFixed}>
-        <Image style={styles.marker} source={markerImage}/>
-      </View>
+        <Image style={styles.marker} source={markerImage}/>    
+      </View> 
       <View style={styles.addButton}>
-        <AppButton title="Add" onPress={null}/>
+        <AppButton title="Add" onPress={markerHandle}/>
       </View>
-      <SafeAreaView style={styles.footer}>
-          <Text style={styles.region}>{JSON.stringify(mapRegion, null, 2)}</Text>
-      </SafeAreaView>   
    </View>
-   
+
   )
 }
 
@@ -150,37 +97,25 @@ const styles = StyleSheet.create({
     width: 200,
     marginBottom: 10,
     height: 10,
-
   },
   markerFixed: {
     left: '50%',
     marginLeft: -24,
     marginTop: -50,
     position: 'absolute',
-    top: '50%'
+    top: '55%'
   },
   map: {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
   },
-  region: {
-    color: '#fff',
-    lineHeight: 20,
-    margin: 20
-  },
-  marker: {
-    height: 30,
-    width: 30
+  marker: { //TODO: This needs to be responsive in regards to the latDelta/longDelta values in the MapView. 
+    height: 48,
+    width: 48
   },
   region: {
     color: '#fff',
     lineHeight: 20,
     margin: 20
-  },
-  footer: {
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    bottom: 0,
-    position: 'absolute',
-    width: '100%'
   },
 })
