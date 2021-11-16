9e184f8cb4e871bb98323bfaf7e8e1a727991830
bam38652 <brian.melgar.364@my.csun.edu>
2021-11-15 21:11:51 -0800

    Integrated Add Screen w/ Ratings & DB Functionality.

--- app/assets/screens/AddScreen.js
+++ app/assets/screens/AddScreen.js
@@ -1,34 +1,85 @@
-import React, { useEffect, useState } from 'react';
-import {
-  StyleSheet,
-  Button,
-  Text,
-  View,
-  Dimensions,
-  SafeAreaView,
-  Image,
-} from 'react-native';
-import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
-import * as Location from 'expo-location';
-import * as Permissions from 'expo-permissions';
-import useLocation from '../../hooks/useLocation';
-import AppButton from '../../components/AppButton';
+import React, { useEffect,useState } from "react";
+import { StyleSheet, Button, Text, View, Dimensions, SafeAreaView,Image,TextInput,TouchableOpacity} from "react-native";
+import MapView, { PROVIDER_GOOGLE, Marker,Circle } from "react-native-maps";
+import * as Location from 'expo-location'
+import AppButton from "../../components/AppButton";
+import {firebase} from "../../../Firebase/firebase"
 import markerImage from '../marker.png';
+import BottomSheet from 'reanimated-bottom-sheet';
+import Rating from "../../components/Rating";
+import {geohashForLocation} from 'geofire-common'
+import { userRating } from "../../components/Rating";
 
 export default function AddScreen() {
-  const [mapRegion, setRegion] = useState(null);
-  const [hasLocationPermissions, setLocationPermission] = useState(false);
-  const [location, setLocation] = useState(null);
+  const [ mapRegion, setRegion ] = useState(null)
+  const [ hasLocationPermissions, setLocationPermission ] = useState( false )
+  const [ location, setLocation] = useState(null);
+  const [title, setTitle] = useState(null);
+  const [description, setDescription] = useState(null);
+  const sheetRef = React.useRef(null);
 
-  useEffect(() => {
-    const getLocationAsync = async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if ('granted' !== status) {
-        setLocation('Permission to access location was denied');
-      } else {
+
+  const renderCont = () =>(
+    <View style={styles.swipeBox}>
+      <Text>Swipe Down To Close</Text>
+      <View style={styles.cont3}>
+        <Text style={styles.title}>Name</Text>
+        <View style={styles.TextInput}>
+            <TextInput
+              label="Title:"
+              onChangeText={(title) => setTitle(title)}
+              placeholder="Name Your Place"
+              mode="outlined"
+              multiline={true}
+            />
+        </View>  
+        <Text style={styles.title}>Description</Text>
+        <View style={styles.TextInput}>     
+          <TextInput
+            label="Description:"
+            onChangeText={(description) => setDescription(description)}
+            placeholder="How Was It?."
+            mode="outlined"
+            multiline={true}
+          />
+        </View>
+        <Text style={styles.title}>Rating</Text>
+        <SafeAreaView style={styles.container1}>
+          <Rating></Rating>
+        </SafeAreaView>
+        <Text style={styles.text}></Text>
+        <View style={styles.cont1}>
+          <TouchableOpacity onPress={addRestroom} style={styles.btn}>
+            <Text style={styles.btnText}>Submit</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+
+
+  //Send Restroom Data to Firestore
+  async function addRestroom() {
+    const dataRef = firebase.firestore().collection('AddedRestrooms');
+    await dataRef.doc(geohashForLocation([mapRegion.latitude, mapRegion.longitude])).set({
+      latitude: mapRegion.latitude,
+      longitude: mapRegion.longitude,
+      description: description,
+      name: title,
+      geohash: geohashForLocation([mapRegion.latitude, mapRegion.longitude]), 
+      rating: userRating
+    });
+  }
+
+
+  useEffect(()=>{ //Must be made into a Hook (Leaving it Here For Now)
+    const getLocationAsync = async () =>{
+      let {status} = await Location.requestForegroundPermissionsAsync();
+      if('granted'!==status){
+        setLocation('Permission to access location was denied')
+      }else{
         setLocationPermission(true);
       }
-
       let {
         coords: { latitude, longitude },
       } = await Location.getLastKnownPositionAsync();
@@ -38,7 +89,7 @@ export default function AddScreen() {
         latitude,
         longitude,
         latitudeDelta: 0.0015,
-        longitudeDelta: 0.0015,
+        longitudeDelta:0.0121,
       }); //Center Map on Location fetched above.
     };
     getLocationAsync();
@@ -59,9 +110,10 @@ export default function AddScreen() {
     return <Text>Map region doesn't exist.</Text>;
   }
 
-  const onRegionChange = (mapRegion) => {
-    setRegion(mapRegion);
-  };
+  const onRegionChange = mapRegion =>{
+    setRegion(mapRegion)
+  }
+
 
   return (
     <View style={styles.container}>
@@ -71,26 +123,40 @@ export default function AddScreen() {
         showsUserLocation={true}
         initialRegion={mapRegion}
         onRegionChangeComplete={onRegionChange}
-      ></MapView>
+      >
+      </MapView>
       <View style={styles.markerFixed}>
         <Image style={styles.marker} source={markerImage} />
       </View>
       <View style={styles.addButton}>
-        <AppButton title="Add" onPress={null} />
+        <AppButton title="Add" onPress={()=>sheetRef.current.snapTo(0)}/>
       </View>
-      <SafeAreaView style={styles.footer}>
-        <Text style={styles.region}>{JSON.stringify(mapRegion, null, 2)}</Text>
-      </SafeAreaView>
-    </View>
-  );
+      <BottomSheet
+        ref={sheetRef}
+        snapPoints={["57%",0]}
+        initialSnap={1}
+        borderRadius={10}
+        renderContent={renderCont}
+        enabledContentTapInteraction={false}
+      />
+   </View>  
+  )
 }
 
 const styles = StyleSheet.create({
+  swipeBox:{
+    backgroundColor: "white",
+    padding: 16,
+    height: 900,
+    alignItems: "center",
+    justifyContent: "flex-end",
+    borderRadius: 40
+  },
   container: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center"
   },
-  addButton: {
+  addButton:{
     position: 'absolute',
     bottom: 50,
     width: 200,
@@ -102,39 +168,79 @@ const styles = StyleSheet.create({
     marginLeft: -24,
     marginTop: -50,
     position: 'absolute',
-    top: '50%',
+    top: '55%'
   },
   map: {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
   },
-  region: {
-    color: '#fff',
-    lineHeight: 20,
-    margin: 20,
+  marker: { //TODO: This needs to be responsive in regards to the latDelta/longDelta values in the MapView. 
+    height: 48,
+    width: 48
   },
-  marker: {
-    height: 30,
-    width: 30,
+  cont1: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    justifyContent: "space-between",
+    marginTop: 40,
+    paddingLeft: 13,
   },
-  region: {
-    color: '#fff',
-    lineHeight: 20,
-    margin: 20,
+  title: {
+    fontSize: 35,
+    marginTop: 30,
+    alignSelf: 'center'
   },
-  footer: {
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    bottom: 0,
-    position: 'absolute',
-    width: '100%',
+  subtitle: {
+    fontSize: 20,
+    color: "#474747",
+    marginTop: 10,
   },
-});
-      {/*
-      <Circle //TODO: This needs to be responsive in regards to the latDelta/longDelta values in the MapView. 
-        center={mapRegion}
-        strokeColor = { '#1a66ff' }
-        strokeWidth = { 1 }
-        radius={20}
-        fillColor = { 'rgba(230,238,255,0.5)' }
-      ></Circle>
-      */}
+  cont3: {
+    flex: 1,
+    backgroundColor: "#FFF",
+    width: "100%",
+    paddingHorizontal: 20,
+  },
+  text: {
+    fontSize: 18,
+    paddingRight: 80,
+    lineHeight: 25,
+  },
+  
+  btn: {
+    backgroundColor: "#E2443B",
+    paddingHorizontal: 40,
+    paddingVertical: 12,
+    borderRadius: 30,
+    width: 300,
+    position: "relative",
+    alignItems: "center",
+  },
+  btnText: {
+    fontSize: 20,
+    color: "#FFF",
+  },
+  TextInput:{
+    height: 200, 
+    borderWidth: 3,
+    borderRadius: 20,
+    paddingTop:10,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  TextInputText:{
+    height: 50, 
+    borderWidth: 3,
+    borderRadius: 20,
+    paddingTop:10,
+    paddingLeft: 10,
+    paddingRight: 10,
+    alignContent: 'center'
+  },
+  container1:{
+    flex: 1,
+    padding: 1,
+    justifyContent: 'center'
+  },
+})
---
+++ app/components/Rating.js
@@ -0,0 +1,70 @@
+import React, {useState} from 'react';
+import {SafeAreaView, StyleSheet, Text, View,Image} from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+var userRating = null // TEMP SOLUTION
+
+export default function Rating (){
+    const [defaultRating, setdefaultRating] = useState(2);
+    const [maxRating, setmaxRating] = useState([1,2,3,4,5])
+    userRating = defaultRating //TEMP SOLUTION
+    const starImgFilled='https://raw.githubusercontent.com/tranhonghan/images/main/star_filled.png'
+    const starImgCorner=  'https://raw.githubusercontent.com/tranhonghan/images/main/star_corner.png'
+
+    const CustomRatingBar = () =>{
+        return(
+            <View style = {styles.customRatingBarStyle}>
+                {
+                    maxRating.map((item,key)=>{
+                        return(
+                            <TouchableOpacity
+                                activeOpacity={0.7}
+                                key={item}
+                                onPress={()=> setdefaultRating(item)}
+                            >
+                                <Image
+                                    style={styles.starImgStyle}
+                                    source={
+                                        item <= defaultRating
+                                        ? {uri: starImgFilled}
+                                        : {uri: starImgCorner}
+                                    }
+                                />
+                            </TouchableOpacity>
+                        )
+                    })
+                }
+            </View>
+        )
+    }
+    return(
+        <SafeAreaView style={styles.container}>
+            <CustomRatingBar>
+            </CustomRatingBar>
+        </SafeAreaView> 
+    )
+};
+
+const styles= StyleSheet.create({
+    container:{
+        flex: 1,
+        padding: 10,
+        justifyContent: 'center'
+    },
+    textStyle:{
+        textAlign: 'center',
+        fontSize: 23
+    },
+    customRatingBarStyle:{
+        justifyContent: 'center',
+        flexDirection:  'row',
+        marginTop: -30
+    },
+    starImgStyle:{
+        width: 40,
+        height: 40,
+        resizeMode: 'cover'
+    }
+});
+
+export {userRating};
