7637c068c8ccd3e91bef5c5b895645f7b1d65b5e
katalla1 <katalla@hotmal.com>
2021-11-17 14:36:57 -0800

    conditional rendering

--- app/assets/screens/FavoritesScreen.js
+++ app/assets/screens/FavoritesScreen.js
@@ -1,15 +1,68 @@
-import React from "react";
-import { StyleSheet, Button, Text, View } from "react-native";
+import React, { useState, useEffect }  from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { firebase } from '../../../Firebase/firebase';
+import { auth } from "../../../Firebase/firebase";
 
- function FavoritesScreen() {
+
+const favoriteRestroomKeys = []
+const favoriteRestrooms = []
+
+  export default function FavoritesScreen() {
+
+    const [favoritesLoaded, setFavoritesLoaded] = useState(false);
+    const [fv, setfv] = useState([]);
+
+    async function getKeyData(){
+      const user = firebase.auth().currentUser;
+      const query = await firebase.firestore().collection('users');
+      query
+      .doc(user.uid)
+      .get().
+      then((querySnapshot) => {
+        const favs = querySnapshot.data();
+          favs.favorites.forEach(favKey => {
+            favoriteRestroomKeys.push(favKey)
+          })
+          getFavoriteData()
+      });
+    }
+    async function getFavoriteData(){
+      const query = await firebase.firestore().collection('Los-Angeles');
+      favoriteRestroomKeys.forEach(key => {
+        console.log(key)
+        query
+        .doc(key)
+        .get().
+        then((querySnapshot) => {
+          console.log(querySnapshot.data())
+          favoriteRestrooms.push(querySnapshot.data());
+        });
+      });
+        setFavoritesLoaded(true); 
+        setfv(favoriteRestrooms);       
+      }
+
+    useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+         if (user) {
+         getKeyData(); 
+         }
+       });
+       return unsubscribe;
+     },[]);
+     
+
+
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text> FavoritesScreen! Welcome! </Text>
+     
+      {favoritesLoaded && fv.map((restroom, index) => (
+          <Text key = {index}>{restroom.name}</Text> 
+      ))}     
     </View>
   );
 }
-
-export default FavoritesScreen;
 
 const styles = StyleSheet.create({
   container: {
