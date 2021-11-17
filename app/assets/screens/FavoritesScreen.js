0d18f4502df77e52113e9eae28fd591478a56274
Ryan Ilano <ryan.ilano.690@my.csun.edu>
2021-11-17 15:29:27 -0800

    Fixed render issue on 'FavoritesScreen.js'

--- app/assets/screens/FavoritesScreen.js
+++ app/assets/screens/FavoritesScreen.js
@@ -1,65 +1,55 @@
-import React, { useState, useEffect }  from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import { firebase } from '../../../Firebase/firebase';
-import { auth } from "../../../Firebase/firebase";
+import { auth } from '../../../Firebase/firebase';
 
+export default function FavoritesScreen() {
+  const [favoritesLoaded, setFavoritesLoaded] = useState(false);
+  const [keys, setKeys] = useState([]);
+  const [fv, setfv] = useState([]);
 
-const favoriteRestroomKeys = []
-const favoriteRestrooms = []
-
-  export default function FavoritesScreen() {
-
-    const [favoritesLoaded, setFavoritesLoaded] = useState(false);
-    const [fv, setfv] = useState([]);
-
-    async function getKeyData(){
-      const user = firebase.auth().currentUser;
-      const query = await firebase.firestore().collection('users');
-      query
+  async function getKeyData() {
+    const user = firebase.auth().currentUser;
+    const query = await firebase.firestore().collection('users');
+    query
       .doc(user.uid)
-      .get().
-      then((querySnapshot) => {
+      .get()
+      .then((querySnapshot) => {
         const favs = querySnapshot.data();
-          favs.favorites.forEach(favKey => {
-            favoriteRestroomKeys.push(favKey)
-          })
-          getFavoriteData()
-      });
-    }
-    async function getFavoriteData(){
-      const query = await firebase.firestore().collection('Los-Angeles');
-      favoriteRestroomKeys.forEach(key => {
-        console.log(key)
-        query
-        .doc(key)
-        .get().
-        then((querySnapshot) => {
-          console.log(querySnapshot.data())
-          favoriteRestrooms.push(querySnapshot.data());
+        favs.favorites.forEach((favKey) => {
+          setKeys((keys) => [...keys, favKey]);
         });
+        getFavoriteData();
       });
-        setFavoritesLoaded(true); 
-        setfv(favoriteRestrooms);       
-      }
+  }
+  async function getFavoriteData() {
+    const query = await firebase.firestore().collection('Los-Angeles');
+    keys.forEach((key) => {
+      console.log(key);
+      query
+        .doc(key)
+        .get()
+        .then((querySnapshot) => {
+          console.log(querySnapshot.data());
+          setfv((fv) => [...fv, querySnapshot.data()]);
+        });
+    });
+    setFavoritesLoaded(true);
+  }
 
-    useEffect(() => {
+  useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
-         if (user) {
-         getKeyData(); 
-         }
-       });
-       return unsubscribe;
-     },[]);
-     
-
-
+      if (user) {
+        getKeyData();
+      }
+    });
+    return unsubscribe;
+  }, [favoritesLoaded]);
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-     
-      {favoritesLoaded && fv.map((restroom, index) => (
-          <Text key = {index}>{restroom.name}</Text> 
-      ))}     
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      {fv &&
+        fv.map((restroom, index) => <Text key={index}>{restroom.name}</Text>)}
     </View>
   );
 }
@@ -67,7 +57,7 @@ const favoriteRestrooms = []
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
