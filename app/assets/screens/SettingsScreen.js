769669bc6603595b29bd97965cebb8669b195701
bam38652 <brian.melgar.364@my.csun.edu>
2021-12-18 15:34:55 -0800

    Settings Screen (Logout Functionality Enabled)

--- app/assets/screens/SettingsScreen.js
+++ app/assets/screens/SettingsScreen.js
@@ -1,10 +1,105 @@
-
-import { Text, View } from "react-native";
-import { firebase } from '../../../Firebase/firebase';
+import { Text, TextInput, View, ScrollView, TouchableOpacity, StyleSheet, Switch, Button, SafeAreaView, Alert } from "react-native";
+import { Auth, firebase } from '../../../Firebase/firebase';
 import React, { useState, useEffect } from 'react';
-import { geohashForLocation, geohashQueryBounds , distanceBetween} from 'geofire-common';
+import colors from "../../assets/config/colors";
+import AppButton from "../../components/AppButton";
+import { auth } from "../../../Firebase/firebase";
+import CustomAlertComponent from "../../components/CustomAlertComponent";
+import { backgroundColor } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
 
-export default function SettingsScreen() {
+
+export default function SettingsScreen({navigation})  { 
+  const user = firebase.auth().currentUser;
+  
+  const Logout=()=>{
+    if(user){
+      firebase.auth().signOut().then(()=>{
+        navigation.navigate("login");
+      })
+    }else{
+      return(
+        <CustomAlertComponent style={styles.container}></CustomAlertComponent>
+      )
+    }
+  } 
+
+  //Do an onpress that  calls a function that has the custom alert component, use imbedded decision JSX to choose when to bring it up.
+
+  const [darkMode, setDarkMode] = useState(false);
+
+    return (
+      <ScrollView style={styles.LogoutButton}> 
+        <View  style={styles.container}>
+          <Text> {'Dark Mode'} </Text>
+          <Switch
+            trackColor={{false: 'grey', true: 'teal'}}
+            thumbColor="white"
+            onValueChange={(value) => setDarkMode(value)}
+            value={darkMode}
+          /> 
+        </View>
+        <View style={{ flex: 1, marginBottom: 10 }}>
+          <TouchableOpacity> 
+          <AppButton title= "LogOut" onPress={Logout}/>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    )
+}
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    //backgroundColor: '#dcdcdc',
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12
+   
+  },
+  container1:{
+    flex: 1,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:'#F5FCFF'
+  },
+  LogoutButton: {
+    paddingTop: 90,
+    padding: 7,  
+    //bottom: 100,
+    //left: 20,
+    //right: 20,
+    height: 30,
+    //marginBottom: 10,  
+    
+  },
+
+  appButtonContainer: {
+    elevation: 8,
+    //backgroundColor: "#dcdcdc",
+    //borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12
+    
+  },
+  appButtonText: {
+    fontSize: 18,
+    color: "#000000",
+    fontWeight: "bold",
+    //alignSelf: "center",
+    //textTransform: "uppercase"
+  }
+});
+
+  
+//});
+
+
+
+
+
  /* const [restrooms, setRestrooms] = useState([]);
   const [restroomsLoaded, setRestroomsLoaded] = useState(false);
 async function getRestrooms() {
@@ -88,16 +183,17 @@ Promise.all(promises).then((snapshots) => {
 useEffect(() =>{
   getData()
 },[])
-*/
+
+
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Settings! Welcome! </Text>
+          {restroomsLoaded && restrooms.map((restroom) => (
          
-  
-                     
+      <Text> {restroom.name} </Text>
+                        ))}
     </View>
   );
 
-    }
-
+*/
