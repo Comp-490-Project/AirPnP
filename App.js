702985223274adf39f10f3238c8c5cff838dc794
bam38652 <brian.melgar.364@my.csun.edu>
2021-12-20 11:42:17 -0800

    Fixes To Make App.js w/ HomeTabs Functional

--- App.js
+++ App.js
@@ -12,11 +12,21 @@ import AddScreen from './app/assets/screens/AddScreen';
 import FavoritesScreen from './app/assets/screens/FavoritesScreen';
 import ReviewScreen from './app/assets/screens/ReviewScreen';
 import SettingsScreen from './app/assets/screens/SettingsScreen';
+import ProfileScreen from './app/assets/screens/ProfileScreen';
+import SplashScreen from './app/assets/screens/SplashScreen'
 
 // Imports from 'AddScreen.js'
 import { firebase } from './Firebase/firebase';
 import { geohashForLocation } from 'geofire-common';
 import { userRating } from './app/components/Rating';
+
+//Imports from 'Tabs.js'
+import { StyleSheet, Dimensions, View, Image, Text, TouchableOpacity } from 'react-native'
+
+
+//Imports from SplashScreen
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import CustomAlertComponent from './app/components/CustomAlertComponent';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -29,6 +39,7 @@ const App = () => {
         <Stack.Screen name="map" component={HomeTabs}></Stack.Screen>
         <Stack.Screen name="forgot" component={ForgotPassword}></Stack.Screen>
         <Stack.Screen name="review" component={ReviewScreen}></Stack.Screen>
+        <Stack.Screen name="add" component={AddScreen}></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -43,6 +54,33 @@ const HomeTabs = ({ navigation }) => {
   const [title, setTitle] = useState(null);
   const [description, setDescription] = useState(null);
   const [mapRegion, setRegion] = useState(null);
+  
+  //For Tabs
+  const Tab = createBottomTabNavigator();
+  const {width,height} = Dimensions.get("window");
+
+  //Function from 'Tabs.js' 
+  const CustomTabBarButton=({children, onPress})=>(
+    <TouchableOpacity
+      style={{
+        top: -20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        ...styles.shadow
+      }}
+      onPress={onPress} 
+    >
+      <View style={{
+        width: 50,
+        height: 50,
+        borderRadius: 35,
+        backgroundColor: colors.tabs
+      }}>
+        {children}
+      </View>
+    </TouchableOpacity>
+  )
+  
 
   // Function from 'AddScreen.js'
   //Send Restroom Data to Firestore
@@ -71,52 +109,27 @@ const HomeTabs = ({ navigation }) => {
     ]);
   }
 
-  return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          if (route.name === 'home') {
-            return (
-              <Ionicons
-                name={focused ? 'home' : 'home-outline'}
-                size={size}
-                color={color}
-              />
-            );
-          } else if (route.name === 'add') {
-            return (
-              <Ionicons
-                name={focused ? 'add-circle' : 'add-circle-outline'}
-                size={size}
-                color={color}
-              />
-            );
-          } else if (route.name === 'favorites') {
-            return (
-              <Ionicons
-                name={focused ? 'star-sharp' : 'star-outline'}
-                size={size}
-                color={color}
-              />
-            );
-          } else if (route.name === 'settings') {
-            return (
-              <Ionicons
-                name={focused ? 'settings' : 'settings-outline'}
-                size={size}
-                color={color}
-              />
-            );
-          }
-        },
-        tabBarInactiveTintColor: colors.medium,
-        tabBarActiveTintColor: colors.tabs,
-      })}
-    >
-      <Tab.Screen
-        name="home"
-        options={{ headerShown: false }}
-        children={() => (
+  return(
+    <View style={{width,height}}>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarShowLabel: false,
+          headerShown: false,  
+          tabBarStyle:[{
+            position:'absolute', 
+            //bottom: 25, 
+            //left: 20, 
+           // right: 20, 
+            elevation: 0, 
+            backgroundColor: '#ffffff',
+            //borderRadius: 15,
+            height: 50,
+            ...styles.shadow
+          }]
+          } 
+        }
+      >
+        <Tab.Screen name = "Home" children={()=>(
           <MapScreen
             keys={keys}
             setKeys={setKeys}
@@ -124,13 +137,43 @@ const HomeTabs = ({ navigation }) => {
             setRestrooms={setRestrooms}
             addRestroom={addRestroom}
             navigation={navigation}
-          />
-        )}
-      />
-      <Tab.Screen
-        name="add"
-        options={{ headerShown: false }}
-        children={() => (
+          ></MapScreen>
+        )} options={{
+          tabBarIcon: ({focused})=>(
+            <View style={{alignItems: 'center', justifyContent: 'center'}}>
+              <Image
+                source={require('./app/assets/Home.png')}
+                resizeMode='contain'
+                style={{
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? colors.tabs : '#748c94'
+                }} 
+              />
+              <Text style={{color: focused ? colors.tabs : '#748c94', fontSize: 12}}>HOME</Text>
+            </View>
+          )
+        }}
+        >
+        </Tab.Screen>
+        <Tab.Screen name = "PROFILE" component={ProfileScreen} options={{
+          tabBarIcon: ({focused})=>(
+            <View style={{alignItems: 'center', justifyContent: 'center'}}>
+              <Image
+                source={require('./app/assets/Profile.png')}
+                resizeMode='contain'
+                style={{
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? colors.tabs : '#748c94'
+                }}
+              />
+              <Text style={{color: focused ? colors.tabs : '#748c94', fontSize: 12}}>PROFILE</Text>
+            </View>
+          )
+        }}
+        />
+        <Tab.Screen name = "AddScreen" children={() => (
           <AddScreen
             mapRegion={mapRegion}
             setRegion={setRegion}
@@ -140,27 +183,76 @@ const HomeTabs = ({ navigation }) => {
             setDescription={setDescription}
             addRestroom={addRestroom}
           />
-        )}
-      />
-      <Tab.Screen
-        name="favorites"
-        options={{ headerShown: false }}
-        children={() => (
-          <FavoritesScreen
-            keys={keys}
-            setKeys={setKeys}
-            navigation={navigation}
-          />
-        )}
-      />
-      <Tab.Screen
-        name="settings"
-        options={{ headerShown: false }}
-        component={SettingsScreen}
-      />
-    </Tab.Navigator>
-  );
-};
+        )} options={{
+            tabBarIcon:({focused}) => (
+              <Image
+                source={require('./app/assets/Add.png')}
+                resizeMode="contain"
+                style={{
+                  width: 25,
+                  height: 25,
+                  tintColor: colors.white
+                }}
+              />  
+            ),
+            tabBarButton: (props) => (
+              <CustomTabBarButton {...props}/>
+            )
+          }}
+        />
+        <Tab.Screen name = "Favorites" component={FavoritesScreen} options={{
+          tabBarIcon: ({focused})=>(
+            <View style={{alignItems: 'center', justifyContent: 'center'}}>
+              <Image
+                source={require('./app/assets/Favorites.png')}
+                resizeMode='contain'
+                style={{
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? colors.tabs : '#748c94'
+                }}
+              />
+              <Text style={{color: focused ? colors.tabs: '#748c94', fontSize: 12}}>FAVORITES</Text>
+            </View>
+          )
+        }}
+        >
+        </Tab.Screen>
+        <Tab.Screen name = "Settings" component={SettingsScreen} options={{
+          tabBarIcon: ({focused})=>(
+            <View style={{alignItems: 'center', justifyContent: 'center'}}>
+              <Image
+                source={require('./app/assets/Settings.png')}
+                resizeMode='contain'
+                style={{
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? colors.tabs : '#748c94'
+                }}
+              />
+              <Text style={{color: focused ? colors.tabs : '#748c94', fontSize: 12}}>SETTINGS</Text>
+            </View>
+          )
+        }}
+        >
+        </Tab.Screen>
+      </Tab.Navigator>
+    </View>
+    )
+  }
+  
+  const styles =  StyleSheet.create({
+    shadow:{
+      shadowColor: '#7F5DF0',
+      shadowOffset:{
+        width:0,
+        height: 10,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 3.5,
+      elevation: 5
+    }
+  })
 
 export { HomeTabs };
 export default App;
