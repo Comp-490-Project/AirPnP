e492dc0c1e64569d02705fbe94528b4ed6a9a05a
MxBM <officialsmp305@gmail.com>
2022-03-27 20:00:15 -0700

    FIX: Tweaked Dimensions and Added Swipe Action

--- app/constants/Dimensions.js
+++ app/constants/Dimensions.js
@@ -3,7 +3,9 @@ const {Dimensions} = require('react-native');
 const {width, height} = Dimensions.get('screen');
 
 export const CARD = {
-    WIDTH: width * 0.9,
-    HEIGHT: height * 0.70,
+    WIDTH: width * 0.95,
+    HEIGHT: height * 0.75,
     BORDER_RADIUS: 20,
 }; 
+
+export const ACTION_OFFSET = 100;
