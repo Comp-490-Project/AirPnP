35b79a36119eb1b52410b73a48f35e5bf026fad5
mnuno96 <80736839+mnuno96@users.noreply.github.com>
2022-05-01 23:16:22 -0700

    login screen ui update

--- app/assets/screens/LoginScreen.js
+++ app/assets/screens/LoginScreen.js
@@ -5,6 +5,7 @@ import {
   Text,
   View,
   TouchableOpacity,
+  Image,
 } from 'react-native';
 import AppButton from '../components/AppButton';
 import AppTextInput from '../components/AppTextInput';
@@ -33,10 +34,14 @@ function LoginScreen({ navigation }) {
   }, []);
 
   return (
-    <ImageBackground
-      style={{ flex: 1, justifyContent: 'center' }}
-      source={require('../../assets/icons/background.jpg')}
-    >
+    <>
+      <View>
+      <Image
+              style={styles.reviewImage}
+              source={require ('../icons/app-final-logo.png')
+              }
+            />
+      </View>
       <Formik
         initialValues={{ email: '', password: '' }}
         onSubmit={(values) => {
@@ -75,9 +80,10 @@ function LoginScreen({ navigation }) {
               />
               <Text style={{ color: 'red' }}>{errors.password}</Text>
             </View>
-            <View style={styles.loginButton}>
+            
+            <View style={styles.signInButton}>
               <TouchableOpacity>
-                <AppButton title="Login" onPress={handleSubmit} />
+                <AppButton title="Sign in" onPress={handleSubmit} />
               </TouchableOpacity>
             </View>
           </>
@@ -85,48 +91,81 @@ function LoginScreen({ navigation }) {
       </Formik>
       <View style={styles.forgot}>
         <Text
-          style={styles.forgot2}
+          style={styles.forgot3}
           onPress={() => navigation.navigate('Forgot')}
         >
-          forgot password?
+          Forgot Password?
         </Text>
       </View>
-      <View style={styles.registerButton}>
-        <AppButton
-          title="Register"
-          onPress={() => navigation.navigate('Register')}
-        />
+
+      <View style={styles.register2}>
+        <Text
+          style={styles.register2}
+        >
+          Don't have an account?
+        </Text>
       </View>
-    </ImageBackground>
+
+      <View style={styles.register}>
+        <Text
+          style={styles.forgot2}
+          onPress={() => navigation.navigate('Register')}
+        >
+          Sign Up
+        </Text>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  loginButton: {
+  signInButton: {
     position: 'absolute',
-    bottom: 100,
-    left: 20,
-    right: 20,
+    bottom: 130,
+    left: 45,
+    right: 45,
     height: 30,
     marginBottom: 10,
+
   },
   forgot: {
     position: 'absolute',
-    bottom: 150,
+    bottom: 75,
     right: 20,
-    width: '25%',
+    width: '60%',
+    fontSize: 10,
   },
   forgot2: {
-    fontSize: 10,
-    color: colors.blue,
+    fontSize: 12,
+    color: colors.primary,
   },
 
-  registerButton: {
+  forgot3:{
+    fontSize: 15,
+    color: colors.primary,
+  },
+
+  register: {
     position: 'absolute',
-    bottom: 50,
-    right: 20,
-    width: '40%',
+    bottom: 7,
+    right: 0,
+    width: '38%',
     height: 30,
+  },
+
+  register2: {
+    position: 'absolute',
+    bottom: 10, 
+    fontSize: 12,
+    color: colors.black,
+    right: 85,
+  },
+
+  reviewImage:{
+    width: 500,
+    top: 85, 
+    height: 250,
+    right: 50,
   },
 
   guestButton: {
@@ -142,6 +181,7 @@ const styles = StyleSheet.create({
     left: 20,
     right: 20,
   },
+
 });
 
 export default LoginScreen;
