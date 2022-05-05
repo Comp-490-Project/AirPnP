f943b90203a654f8436d0f74e247029354d0e060
officialsmp305 <Officialsmp305@gmail.com>
2022-05-05 02:55:16 -0700

    [REFACTOR]: Styled AddScreen.

--- app/assets/screens/AddScreen.js
+++ app/assets/screens/AddScreen.js
@@ -1,100 +1,190 @@
-import React, { useEffect, useRef } from 'react';
-import { StyleSheet, View, Dimensions, Image } from 'react-native';
-import MapView from 'react-native-maps';
+import React, { useEffect, useState, useRef } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity, Image} from 'react-native';
 import AppButton from '../components/AppButton';
-import markerImage from '../icons/marker.png';
-import AnimationLoad from '../components/AnimationLoad';
-import CustomAlertComponent from '../components/CustomAlertComponent';
-import AddBottomSheet from '../components/bottomSheets/AddBottomSheet';
-
-import { getUserLocation } from '../../actions/userActions';
 import { setRestroomLocation } from '../../actions/restroomActions';
-import { useSelector, useDispatch } from 'react-redux';
+
+
+import mapStyle from '../../constants/mapStyle'
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+import {useSelector, useDispatch} from 'react-redux'
+import {getUserLocation} from '../../actions/userActions'
+import AnimationLoad from '../components/AnimationLoad';
+import { HEIGHT, WIDTH } from '../../constants/Dimensions';
+import colors from '../theme/colors';
+import {isPointWithinRadius} from 'geolib'
+import ModalAlert from '../components/ModalAlert';
+import SafeView from '../components/SafeView';
+import LightText from '../components/LightText';
+import SubmitScreen from './SubmitScreen';
+
+
 
 function AddScreen({ navigation }) {
-  const reference = useRef(null);
-
   const dispatch = useDispatch();
+  const {user} = useSelector((state) => state.userAuth);
+  const {location} = useSelector((state) => state.userLocation);
+  const {region} = useSelector((state) => state.restroomReview);
 
-  const { location } = useSelector((state) => state.userLocation);
-  const { user } = useSelector((state) => state.userAuth);
-  const { region } = useSelector((state) => state.restroomReview);
+  const [visible, setVisible] = useState(false);
+  const [radius, setRadius] = useState();
 
-  useEffect(() => {
-    if (!location) {
+
+  function navigate(){
+    navigation.navigate("Add");
+  }
+
+  function validate(){ //Returns boolean based on geolib calculation.
+    const radius = isPointWithinRadius(
+      {latitude: region.latitude, longitude: region.longitude },
+      {latitude: location.latitude, longitude: location.longitude},
+      50
+    )
+
+    if(radius){
+      console.log('IN RADIUS')
+      setRadius(true)
+    }else{
+      console.log('NOT IN RADIUS');
+      setRadius(false);
+      setVisible(true);
+    }
+  }
+  
+  useEffect(()=>{ //Fetch location, catch err.
+    if(!location){
       dispatch(getUserLocation());
-    } else if (!region.latitude || !region.longitude) {
+    } else if (!region.latitude || !region.longitude){
       dispatch(
         setRestroomLocation({
           latitude: location.latitude,
           longitude: location.longitude,
-          latitudeDelta: 0.015,
-          longitudeDelta: 0.015,
+          latitudeDelta: 0.002,
+          longitudeDelta: 0.002,
         })
-      );
+      )
     }
-  }, [location, region]);
+    <View>
+      <AnimationLoad></AnimationLoad>
+    </View>
+  },[location,region])
 
-  if (!region.latitude || !region.longitude) {
-    return <AnimationLoad />;
+
+  if(!region.latitude || !region.longitude){
+    return <AnimationLoad></AnimationLoad>
+  }
+
+  if (!user) { //Prompt user to log in if they aren't 
+    return (
+      <SafeView>
+        <View style={styles.contentContainer}>
+          <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+            <View style={styles.btnLogin}>
+              <LightText fontSize={24}>Login</LightText>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </SafeView>
+    );
   }
 
   return (
-    <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        showsMyLocationButton={true}
-        showsUserLocation={true}
-        initialRegion={region}
-        onRegionChangeComplete={(region) =>
-          dispatch(setRestroomLocation(region))
-        }
-      ></MapView>
-      <View style={styles.markerFixed}>
-        <Image style={styles.marker} source={markerImage} />
-      </View>
-      {user ? (
+    <>
+    {radius ? (
+      <SubmitScreen/>
+      ):(
+        <View style={{flex: 1, justifyConetnt: 'center', alignItems: 'center'}}>
+          <ModalAlert visible={visible}>
+            <View style={{alignItems:'center'}}> 
+              <View style={styles.header}>
+                <TouchableOpacity onPress={() => setVisible(false)}>
+                  <Image
+                    source={require('../icons/cancel.png')}
+                    style={{height: 30, width: 30}}
+                  />
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View style={{alignItems: 'center'}}>
+              <Image
+                source={require('../icons/poop-emoji.png')}
+                style={{height: 100, width: 100, marginVertical:  10}}
+              />
+            </View>
+            <Text style={{marginVertical: 30, fontSize: 20, textAlign: 'center'}}>
+              Try again, the position is too far from the allowed radius. 
+            </Text>
+          </ModalAlert>
+          <MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.mapView}
+          showsUserLocation={true}
+          showsMyLocationButton={false}
+          customMapStyle={mapStyle}
+          loadingEnabled={true}
+          initialRegion={region}
+          onRegionChangeComplete={(region)=>
+            dispatch(setRestroomLocation(region))
+          }
+          
+        >
+          <MapView.Circle
+            center = {location}
+            radius={50}
+            fillColor ={'rgba(149, 153, 226, 0.5)'} 
+            stokeWidth={5}
+          >
+          </MapView.Circle>
+          <Marker.Animated
+            coordinate={(region)}    
+          >
+          </Marker.Animated>
+        </MapView>
         <View style={styles.addButton}>
-          <AppButton title="Add" onPress={() => reference.current.snapTo(0)} />
+          <AppButton onPress={validate} title="Validate"></AppButton>
         </View>
-      ) : (
-        <CustomAlertComponent navigation={navigation} />
-      )}
-      <AddBottomSheet reference={reference} navigation={navigation} />
-    </View>
+        </View>
+    )}  
+
+    </>
+
   );
 }
 
+
+  
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
+  mapView:{
+    width: WIDTH,
+    height: HEIGHT*0.88, 
   },
-  map: {
-    flex: 1,
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
-  markerFixed: {
-    left: '50%',
-    marginLeft: -24,
-    marginTop: -50,
-    position: 'absolute',
-    top: '55%',
-  },
-  marker: {
-    // @todo
-    // This needs to be responsive in regards to the latDelta/longDelta values in the MapView
-    height: 48,
-    width: 48,
-  },
-  addButton: {
+  addButton:{
     position: 'absolute',
     bottom: 20,
-    width: 200,
+    width: "100%",
     marginBottom: 125,
     height: 10,
   },
+  header: {
+    width: '100%',
+    height: 40,
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+  },
+  contentContainer: {
+    flex: 1,
+    height: HEIGHT - 50,
+    maxWidth: WIDTH,
+    paddingHorizontal: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  btnLogin: {
+    backgroundColor: '#303645',
+    paddingVertical: 15,
+    paddingHorizontal: 75,
+    borderRadius: 5,
+  },
+
 });
 
 export default AddScreen;
