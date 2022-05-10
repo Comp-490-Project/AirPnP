0696ea38a3d4961f3b98e233c612914332ecd6e2
officialsmp305 <Officialsmp305@gmail.com>
2022-05-09 18:02:02 -0700

    [FRONTEND]: Added Images.

--- app/constants/slides.js
+++ app/constants/slides.js
@@ -3,16 +3,20 @@ export default [
         id: '1',
         title: 'Find Them',
         description: 'Automatically get routed to the nearest restroom',
+        image: require("C:/AirPnP-2/app/assets/icons/world.png")
     },
     {
         id: '2',
         title: 'Add Them',
         description: 'Add Restrooms To Our Database and Help Map Out Your Community!',
+        image: require("C:.AirPnP-2/app/assets/icons/add.png")
+
     },
     {
         id: '3',
         title: 'Post Them',
         description: 'Give Others Insight On The State Of Your Local Restrooms and Connect In Ways You Never Thought Feasible',
+        image: require("")
     },
     {
         id: '4',
